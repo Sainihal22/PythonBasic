1e3e45b2907b0791da4ad166e0d60fb1983b0265
Sai_nihal_Kothi <Sai_nihal.Kothi@dell.com>
2026-03-14 23:53:32 +0530

    Armstrong and Prime Number

--- Interview.py
+++ Interview.py
@@ -44,3 +44,93 @@
 #     print("Not a Pallindrome Number")
 
 # 4. Numbers divisible by 3 & 5
+
+#print even number 1 to 50 using loops
+
+# for i in range (0,51):
+#   if i%2 == 0:
+#       print(i)
+
+# x=10
+# y=50
+# z=30
+
+# largest = x
+# if y >largest:
+#     largest=y
+# if z >largest:
+#     largest=z
+
+
+# print(largest)
+
+# for i in range(0,51):
+#     if i%3==0 and i%2==0:
+#         print(i)
+
+#fibonacci series
+# a=0
+# b=1
+# for i in range (10):
+#     temp=a+b
+#     print(a)
+#     a=b
+#     b=temp
+
+#factorial
+# fac=1
+# n=int(input("enter a number to perform factorial:"))
+# for i in range(1,n+1):
+#     fac=i*fac
+# print(fac)
+
+#counting the number of digits in a number input 12345 output 5
+n=int(input("enter a number:"))
+count = 0
+while n>0:
+    n = n//10
+    count+=1
+
+
+print(count)
+#check for a prime number
+#prime numbers: divisble by 1 and itself
+
+
+#prime number
+
+# P = True
+# n=int(input("give an number:"))
+# for i in range(2,int(((n**0.5)+1))):
+#     if n%i==0:
+#         P = False
+#         break
+# if P==True:
+#     print(f"{n} is Prime")
+# else:
+#     print(f"{n} is notPrime")
+
+#armstrong
+
+# M=int(input("Enter an number:"))
+# N=M
+# S=M
+# count=0
+# dig=0
+# while N>0:
+#     N=N//10
+#     count=count+1
+# print(f"{count} is count")
+# print(f"{N} is N")
+# print(f"{M} is M")
+# while M>0:
+#     rem=M%10
+#     print(rem)
+#     dig = (rem**count) + dig
+#     M = M//10
+# print(f"{dig} is dig")
+# print(f"{M} is M")
+# if S==dig:
+#     print(f"{S} is a Armstrong Number")
+# else:
+#     print(f"{S} is not an Armstrong Number")
