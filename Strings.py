d1e01297ed1ef94564b3bf9705cbca30e67f6232
Sai_nihal_Kothi <Sai_nihal.Kothi@dell.com>
2026-03-19 00:58:59 +0530

    Strings

--- Strings.py
+++ Strings.py
@@ -4,5 +4,11 @@
 # 2. We can using slicing and indexing
 # 3. We can traverse using loops
 
-Name = "Python"
-print(Name)
+Name1 = "Nihal"
+# Name2 = 'Nihal'
+# Name3 = """My Name is Nihal"""
+# print(Name1)
+# print(Name2)
+# print(Name3)
+# print(Name1[2])
+print(Name1[0:3:2])
