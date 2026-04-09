04302e8d2b3e37b6ca7fb88bf0ba40c4421b5ac6
Sai_nihal_Kothi <Sai_nihal.Kothi@dell.com>
2026-04-09 20:15:21 +0530

    Dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,55 +1,101 @@
-# Dictionary
-# It is a collection of key and value pairs
+# Dictionaries
+
+# It stores data in key value pairs
+# E.g : Phone book
+
+# Key properties
+
+# 1. Key Value Pairs : Every item will have a key and a value
+# 2. Keys are unique : No duplicate keys
+# 3. Mutable : Can add, remove, change
+# 4. Ordered : while inserting an element order is being maintained (3.7+)
+# 5. Keys are hashable : Keys must be immutable
+
+# Valid and Invalid Keys
+
+# d = {
+#     "name" : "Soundarya",
+#     1 : "one",
+#     3.14 : "pi",
+#     True : "yes",
+#     "BMS" : 1,
+#     (1,2) : "Tuple"
+# }
+# print(d)
+
+# Invalid
+# d = {
+#     {"A": 1, "B" : 2} : "Dict",
+#     {1,2} : "Sets",
+#     [1,2] : "List"
+# }
+# print(d)
+
+# Creating a dictionary
+
+# Method 1 : Curly braces
 
 # student = {
-#     "name" : "Amit",
-#     "age" : 20,
+#     "name" : "Alice",
+#     "age" : 22,
 #     "branch" : "CSE"
 # }
-# list of keys
-# print(student.keys())
-
-# for key in student.keys():
-#     print(key)
-
-# for value in student.values():
-#     print(value)
-
-# for key, value in student.items():
-#     print(key, value)
-
 # print(student)
 
-# Creating
-# d = {}
-# d = dict()
+# Method 2 : dict() constructor
 
-# Accessing
-# print(student["name"], student["section"])
-
-# print(student.get("section", "A"))
-
-# Add / Update
-# Add
-# student["college"] = "BMSIT"
+# student = dict(name = "Soundarya", age = 22, branch = "ECE")
 # print(student)
 
-# Update
+# Method 3 : Empty then Add
+
+# student = {}
+# student["name"] = "Soundarya"
+# student["age"] = 23
 # student["branch"] = "ECE"
 # print(student)
 
-# Remove
-# student.pop("branch")
-# print(student)
-# student.popitem()
-# print(student)
-# del student["section"]
+# Method 4 : From two lists using zip()
+
+# keys = ["name", "age", "branch"]
+# values = ["Soundarya", 22, "ECE"]
+
+# student = dict(zip(keys, values))
 # print(student)
 
-# Nested Dictionary
-students = {
-    "s1": {"name": "Nihal", "age": 20},
-    "s2": {"name": "Rahul", "age": 21}
-}
+# Accessing
 
-print(students["s1"]["name"])
+# student = {
+#     "name" : "Alice",
+#     "age" : 22,
+#     "branch" : "CSE"
+# }
+# print(student["name"])
+# print(student.get("name"))
+
+# print(student["section"])
+# print(student.get("section", "No Key present"))
+
+# Adding new key value pairs
+# student = {"name" : "Soundarya", "age" : 22}
+# print(student)
+# student["branch"] = "ECE"
+# print(student)
+
+# Updating
+# student = {"name" : "Soundarya", "age" : 22}
+# print(student)
+# student["age"] = 23
+# print(student)
+
+# Get all keys
+# student = {"name" : "Soundarya", "age" : 22}
+# print(student.keys())
+# keys = list(student.keys())
+# print(keys)
+
+# update() : Merge or update
+student = {"name" : "Soundarya", "age" : 22}
+place = {"city" : "Bengaluru", "branch": "ECE"}
+student.update(place)
+print(student)
